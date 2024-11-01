3e6799031538579d3692c2b0eae154fbd2f1c137
Chris Gray <chris.gray@aem.eco>
2024-11-01 14:24:57 -0700

    First pass on the challenge.
    On my computer, I get 73k logs / s on the sync challenge and 228 logs
    / s on the async challenge.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,28 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+// Find the source with the least recent entry.
+function compareSources(a, b) {
+  if (a.last.date < b.last.date) {
+    return a;
+  }
+  return b;
+}
+
+function findSourceWithLeastRecentEntry(sources) {
+  return sources.reduce((a, b) => compareSources(a, b));
+}
+
+module.exports = async (logSources, printer) => {
+  while (logSources.length !== 0) {
+    const source = findSourceWithLeastRecentEntry(logSources);
+    printer.print(source.last);
+    const log = await source.popAsync();
+    // Remove sources from the list when they are drained.
+    if (!log) {
+      const index = logSources.indexOf(source);
+      logSources.splice(index, 1);
+    }
+  }
+  printer.done();
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,28 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+// Find the source with the least recent entry.
+function compareSources(a, b) {
+  if (a.last.date < b.last.date) {
+    return a;
+  }
+  return b;
+}
+
+function findSourceWithLeastRecentEntry(sources) {
+  return sources.reduce((a, b) => compareSources(a, b));
+}
+
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  while (logSources.length !== 0) {
+    const source = findSourceWithLeastRecentEntry(logSources);
+    printer.print(source.last);
+    source.pop();
+    // Remove sources from the list when they are drained.
+    if (source.drained) {
+      const index = logSources.indexOf(source);
+      logSources.splice(index, 1);
+    }
+  }
+  printer.done();
 };
