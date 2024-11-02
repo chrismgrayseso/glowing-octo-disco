82463b443d965af7f388f38bdbfc71fa6f344206
Chris Gray <chrismgray+seso@gmail.com>
2024-11-01 21:06:05 -0700

    Third pass.
    This improves the speed of the async case by several orders of
    magnitude, depending on the amount of memory available.  We do this by
    popping as many sources as possible at once, removing the need to wait
    after popping each one.
    
    Async logs / s with 100 sources is 11k.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,32 +1,88 @@
 "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+const MinHeap = require("./heap");
+
+// Initialize the heap and capacities map.
+async function fillHeap(logSources) {
+  const heap = new MinHeap(compareSources);
+  // Maintains the number of log entries per source in the heap.
+  // We must never let an un-drained source have 0 entries in the heap,
+  // otherwise we run the risk that we print logs out of order.
+  const capacities = new Map();
+  for (const logSource of logSources) {
+    capacities.set(logSource, 0);
+  }
+  await fillHeapNTimes(
+    logSources,
+    MAX_LOGS_PER_SOURCE_IN_HEAP,
+    heap,
+    capacities,
+  );
+  return { heap, capacities };
+}
 
 // Find the source with the least recent entry.
 function compareSources(a, b) {
-  if (a.last.date < b.last.date) {
-    return a;
+  if (a.log.date < b.log.date) {
+    return -1;
   }
-  return b;
+  return 1;
 }
 
-function findSourceWithLeastRecentEntry(sources) {
-  return sources.reduce((a, b) => compareSources(a, b));
+// This constant can be tweaked to increase speed at the expense of memory.
+const MAX_LOGS_PER_SOURCE_IN_HEAP = 100;
+
+// Maintain MEDIUM + MIN = MAX
+const MEDIUM_LOGS_PER_SOURCE_IN_HEAP = 95;
+const MIN_LOGS_PER_SOURCE_IN_HEAP = 5;
+
+// For the log sources in the logSources array, add each of their logs
+// to the heap n times.  Crucially, retrieve all the logs for all the sources
+// at once, to minimize the wait time associated with popAsync.
+async function fillHeapNTimes(logSources, n, heap, capacities) {
+  for (let i = 0; i < n; ++i) {
+    await Promise.all(
+      logSources.map((s) => {
+        capacities.set(s, capacities.get(s) + 1);
+        if (!s.drained) {
+          heap.add({ source: s, log: s.last });
+          return s.popAsync();
+        }
+      }),
+    );
+  }
+}
+
+
+// Print the most recent log and maintain the capacities map.
+async function printLogs(heap, capacities, printer) {
+  const { source, log } = heap.remove();
+  printer.print(log);
+  capacities.set(source, capacities.get(source) - 1);
+  // Do not add to the heap every time a log is printed.  Instead,
+  // wait until one of the sources has too low of a capacity and then
+  // hopefully many sources can be added at once.
+  if (capacities.get(source) < MIN_LOGS_PER_SOURCE_IN_HEAP) {
+    const mediumCapacitySources = [];
+    for (const key of capacities.keys()) {
+      if (capacities.get(key) < MEDIUM_LOGS_PER_SOURCE_IN_HEAP) {
+        mediumCapacitySources.push(key);
+      }
+    }
+    await fillHeapNTimes(
+      mediumCapacitySources,
+      MIN_LOGS_PER_SOURCE_IN_HEAP,
+      heap,
+      capacities,
+    );
+  }
 }
 
 module.exports = async (logSources, printer) => {
-  while (logSources.length !== 0) {
-    const source = findSourceWithLeastRecentEntry(logSources);
-    const lastLog = source.last;
-    // A slight optimization -- start the promise while the printer is printing.
-    const promise = source.popAsync();
-    printer.print(lastLog);
-    const log = await promise;
-    // Remove sources from the list when they are drained.
-    if (!log) {
-      const index = logSources.indexOf(source);
-      logSources.splice(index, 1);
-    }
+  const { heap, capacities } = await fillHeap(logSources);
+
+  while (!heap.isEmpty()) {
+    await printLogs(heap, capacities, printer);
   }
   printer.done();
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,112 +1,9 @@
 "use strict";
 
+const MinHeap = require("./heap");
+
 // Print all entries, across all of the sources, in chronological order.
 
-// A standard min-heap.  Some implementation taken from
-// https://www.geeksforgeeks.org/min-heap-in-javascript/
-//
-// In a more standard situation, I might reach for a library instead,
-// such as https://github.com/ignlg/heap-js
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-  // Helper Methods
-  getLeftChildIndex(parentIndex) {
-    return 2 * parentIndex + 1;
-  }
-  getRightChildIndex(parentIndex) {
-    return 2 * parentIndex + 2;
-  }
-  getParentIndex(childIndex) {
-    return Math.floor((childIndex - 1) / 2);
-  }
-  hasLeftChild(index) {
-    return this.getLeftChildIndex(index) < this.heap.length;
-  }
-  hasRightChild(index) {
-    return this.getRightChildIndex(index) < this.heap.length;
-  }
-  hasParent(index) {
-    return this.getParentIndex(index) >= 0;
-  }
-  leftChild(index) {
-    return this.heap[this.getLeftChildIndex(index)];
-  }
-  rightChild(index) {
-    return this.heap[this.getRightChildIndex(index)];
-  }
-  parent(index) {
-    return this.heap[this.getParentIndex(index)];
-  }
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-
-  // Functions to create Min Heap
-
-  swap(indexOne, indexTwo) {
-    const temp = this.heap[indexOne];
-    this.heap[indexOne] = this.heap[indexTwo];
-    this.heap[indexTwo] = temp;
-  }
-
-  peek() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-    return this.heap[0];
-  }
-
-  // Removing an element will remove the
-  // top element with highest priority then
-  // heapifyDown will be called
-  remove() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-    const item = this.heap[0];
-    this.heap[0] = this.heap[this.heap.length - 1];
-    this.heap.pop();
-    this.heapifyDown();
-    return item;
-  }
-
-  add(item) {
-    this.heap.push(item);
-    this.heapifyUp();
-  }
-
-  heapifyUp() {
-    let index = this.heap.length - 1;
-    while (
-      this.hasParent(index) &&
-      compareSources(this.parent(index), this.heap[index]) > 0
-    ) {
-      this.swap(this.getParentIndex(index), index);
-      index = this.getParentIndex(index);
-    }
-  }
-
-  heapifyDown() {
-    let index = 0;
-    while (this.hasLeftChild(index)) {
-      let smallerChildIndex = this.getLeftChildIndex(index);
-      if (
-        this.hasRightChild(index) &&
-        compareSources(this.rightChild(index), this.leftChild(index)) < 0
-      ) {
-        smallerChildIndex = this.getRightChildIndex(index);
-      }
-      if (compareSources(this.heap[index], this.heap[smallerChildIndex]) < 0) {
-        break;
-      } else {
-        this.swap(index, smallerChildIndex);
-      }
-      index = smallerChildIndex;
-    }
-  }
-}
 
 // Find the source with the least recent entry.
 function compareSources(a, b) {
@@ -116,12 +13,8 @@ function compareSources(a, b) {
   return 1;
 }
 
-function findSourceWithLeastRecentEntry(sources) {
-  return sources.reduce((a, b) => compareSources(a, b));
-}
-
 module.exports = (logSources, printer) => {
-  const heap = new MinHeap();
+  const heap = new MinHeap(compareSources);
   for (const logSource of logSources) {
     heap.add(logSource);
   }
