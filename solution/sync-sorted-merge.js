d34bd86c382679f078ff2f3d07ec8f2b47f88999
Chris Gray <chrismgray+seso@gmail.com>
2024-11-02 21:17:32 -0700

    Fix a bug where the last message from each source would not get printed

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -4,7 +4,6 @@ const MinHeap = require("./heap");
 
 // Print all entries, across all of the sources, in chronological order.
 
-
 // Find the source with the least recent entry.
 function compareSources(a, b) {
   if (a.last.date < b.last.date) {
@@ -21,8 +20,8 @@ module.exports = (logSources, printer) => {
 
   while (!heap.isEmpty()) {
     const source = heap.remove();
+    printer.print(source.last);
     if (source && !source.drained) {
-      printer.print(source.last);
       source.pop();
       heap.add(source);
     }
